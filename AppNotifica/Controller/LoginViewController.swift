38a8b7489c0b3372c613918bbc54abd5c9504180
who1sthais <thais61204@estudante.ifb.edu.br>
2023-11-27 10:13:19 -0300

    video 3

--- AppNotifica/Controller/LoginViewController.swift
+++ AppNotifica/Controller/LoginViewController.swift
@@ -18,7 +18,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Entrar"
+        self.title = "Logar"
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
